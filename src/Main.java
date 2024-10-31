52e04cd8b77529954b6cf7a9ef5c69f60bc22ddf
Julieta Watts <clariflaro@gmail.com>
2024-10-31 13:26:48 -0300

    Código terminado.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Programa de Blancanieves");
+        //IMPORTANTE: el sleep no libera el monitor, el wait sí. Siempre que usamos wait y notify usamos synchorinize.
+
+
+    }
+}
